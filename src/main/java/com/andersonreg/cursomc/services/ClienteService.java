d54d038f46e28ac96f4496d7773692fa20a98214
Anderson Reginaldo <anderson.reginaldo@gmail.com>
2021-12-30 17:28:05 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/andersonreg/cursomc/services/ClienteService.java
@@ -0,0 +1,21 @@
+package com.andersonreg.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andersonreg.cursomc.domain.Cliente;
+import com.andersonreg.cursomc.repositories.ClienteRepository;
+import com.andersonreg.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
