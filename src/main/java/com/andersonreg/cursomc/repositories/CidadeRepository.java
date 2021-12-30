453a0dcbf2c6a31e0e5b1e736304665c150567da
Anderson Reginaldo <anderson.reginaldo@gmail.com>
2021-12-30 15:22:47 -0300

    Estado e Cidade

---
+++ src/main/java/com/andersonreg/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.andersonreg.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.andersonreg.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
